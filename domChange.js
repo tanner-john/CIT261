1872aafd097e2d02b5bd718244b44797fa814079
John Tanner <thecoderhero@gmail.com>
2019-02-05 20:30:36 -0700

    Game Mechs
    Fixed all Game Mechanics. Game is fully functional.

--- domChange.js
+++ domChange.js
@@ -22,17 +22,23 @@ var oej2 = new Card('One Eyed Jack 2', 5, 'oej2','pics/oneeyedjack.png');
 
 //Game variables
 var cards = new Array(as1, as2, ac1, ac2, ah1, ah2, ad1, ad2, sk1, sk2, oej1, oej2);
-var secondFlip = false;
+var flipCounter = false;
 var flipOne = 0;
 var flipTwo = 0;
 var totalFlips = 10;
 var points = 0;
+var cardID1 = '';
+var cardID2 = '';
+var eventTarget1 = '';
+var eventTarget2 = '';
+var timer;
+
 
 function boardSetup(){
     console.log('boardStup() function called!');
     document.getElementById('playButton').style.display = 'none';
     for(var x = 0; x < cards.length; x++){
-        document.getElementById('card' + x).innerHTML = "<div class='flip-card w3-margin' id='flip-card" + x + "'><div class='flip-card-inner' id='flip-card-inner" + x + "'><div class='flip-card-front'><img src='pics/cardBack.png' alt='Back of Playing Card' class='cardImg' onclick='flipCard(event)'></div><div class='flip-card-back'><img src='" + cards[x].img + "' alt='" + cards[x].name + "' class='cardImg' onclick='flipCardBack(event)'></div></div></div>";
+        document.getElementById('card' + x).innerHTML = "<div class='flip-card w3-margin' id='flip-card" + x + "'><div class='flip-card-inner' id='flip-card-inner" + x + "'><div class='flip-card-front'><img src='pics/cardBack.png' alt='Back of Playing Card' class='cardImg' onclick='flipCard(event)' id='" + cards[x].id + "'></div><div class='flip-card-back'><img src='" + cards[x].img + "' alt='" + cards[x].name + "' class='cardImg'></div></div></div>";
     }
 }
 
@@ -51,64 +57,88 @@ function shuffle(array) {
         array[m] = array[i];
         array[i] = t;
     }
+    console.log('Cards Shuffled!');
     return array;
 }
 
 function consoleCards(array){
     for(x in array){
-        console.log(array[x].name);
-        console.log(array[x].value);
-    }
-}
-
-//Determine which flip we are evaluating
-function isFirstFlip(){
-    if(!secondFlip){
-        firstFlip();
-    }
-    else if(secondFlip){
-        secondFlip();
-    }
-    else{
-        console.log('Something went wrong in isFirstFlip() function!');
+        console.log(x + '. ' + array[x].name);
     }
 }
 
 //Store value of first card flipped
-function firstFlip(){
-    flipOne = 1 //Value of card flipped.
-    secondFlip = true;
+function firstFlip(id){
+    stopTimer();
+    for(x in cards){
+        if(cards[x].id == id){
+            flipOne = cards[x].value;
+            cardID1 = cards[x].id;
+            console.log(cardID1);
+        }
+    }
+    flipCounter = true;
+    console.log('firstFlip flipOne ' + cardID1);
 }
 
 //Store value of second card flipped and check match
-function secondFlip(){
-    flipTwo = 2 //Value of second card flipped
-    checkMatch(flipOne, flipTwo);
+function secondFlip(id){
+    for(x in cards){
+        if(cards[x].id == id){
+            flipTwo = cards[x].value;
+            cardID2 = cards[x].id;
+            console.log(cardID2);
+        }
+    }
+    flipCounter = false;
+    console.log('firstFlip flipTwo ' + cardID2);
+    checkMatch();
 }
 
 //Check the cards for a match
-function checkMatch(cardA, cardB){
-    if(cardA.value == cardB.value){
-        //disable flip animation of both DOM elements
-        //Remove Dom Elements
-        //Increase Score by 1
-        //Decrease flips by 1
+function checkMatch(){
+    console.log('Flip One = ' + flipOne);
+    console.log('Flip Two = ' + flipTwo);
+    if(flipOne == flipTwo){
+        points++;
+        console.log('Match!');
     }
     else{
-        //Run flip animation of both DOM elements
-        //Decrease flips by 1
+        timer = setTimeout(function(){
+            eventTarget1.style.transform = 'rotateY(0deg)';
+            eventTarget2.style.transform = 'rotateY(0deg)';
+        }, 2000);
+        totalFlips--;
     }
-    secondFlip = false;
+    document.getElementById('points').innerHTML = ' ' + points;
+    document.getElementById('attempts').innerHTML = ' ' + totalFlips;
+    if(points == 6){
+        winCondition();
+    }
 }
 
 function flipCard(event){
     document.getElementById(event.target.parentElement.parentElement.id).style.transform = 'rotateY(180deg)';
+    var cardId = event.target.id;
+    console.log(cardId + '- Flipped');
+    if(!flipCounter){
+        eventTarget1 = document.getElementById(event.target.parentElement.parentElement.id);
+        firstFlip(cardId);
+    }
+    else{
+        eventTarget2 = document.getElementById(event.target.parentElement.parentElement.id);
+        secondFlip(cardId);
+    }
 }
 
-function flipCardBack(event){
-    document.getElementById(event.target.parentElement.parentElement.id).style.transform = 'rotateY(0deg)';
+function stopTimer(){
+    clearTimeout(timer);
 }
 
 //Game Operation Functions "Actual Gameplay"
 shuffle(cards);
 consoleCards(cards);
+
+function winCondition(){
+    
+}
